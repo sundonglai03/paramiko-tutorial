d52e17a873cbf1d2eb5a5aadb1a7fd30816252a0
sundonglai03 <sundonglai03@gmail.com>
2026-09-10 17:31:57 +0800

    refactor: migrate project to ssh_mcp package

--- tests/test_imports.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-
-def test_ssh_tool_package_is_importable():
-    module = importlib.import_module("ssh_tool")
-    assert hasattr(module, "upload_directory")
-    assert hasattr(module, "execute_remote_commands")
-    assert hasattr(module, "RemoteClient")
--- tests/test_mcp_server.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-
-def test_mcp_server_module_exists():
-    module = importlib.import_module("ssh_tool.mcp_server")
-    assert hasattr(module, "create_mcp_server")
-    assert hasattr(module, "ssh_execute_command")
-    assert hasattr(module, "ssh_upload_directory")
